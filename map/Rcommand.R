223f7e19baa1a7f6ad80929f992c69910cbafac3
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2021-06-14 11:34:46 -0400

    few edits

--- map/Rcommand.R
+++ map/Rcommand.R
@@ -1,9 +1,9 @@
 
-setwd('~/Desktop/SAR_metadata')
+setwd('~/Documents/GitHub/SAR_dark_area-codes/map')
 require (rworldmap)
 require(rworldxtra)
 dataGB <- read.table("map_gps_speciesv2.txt", sep="\t", stringsAsFactors = FALSE, quote = "", header = TRUE)
 attach(dataGB)
-mapPies( dataGB,nameX="LON", nameY="LAT", nameZs=c('Globigerina_bulloides','Globigerinella_siphonifera','Amphisorus_hemprichii','Phyllostaurus_cuspidatus','Globoquadrina_conglomerata','Virgulinella_fragilis','Sorites_orbiculus','Neidium_amphigomphus'),mapRegion='world', oceanCol = "white",landCol = "gray80")
+mapPies( dataGB,nameX="LON", nameY="LAT", nameZs=c('Globigerinella_siphonifera','Globigerinoides_sacculifer','Globigerinoides_ruber','Globigerina_bulloides','Turborotalita_quinqueloba','Globigerinella_calida','Orbulina_universa','Pulleniatina_obliquiloculata','Neogloboquadrina_pachyderma','Globorotalia_inflata','Neogloboquadrina_incompta','Globigerinita_uvula','Globorotalia_truncatulinoides','Globorotalia_scitula','Globorotalia_hirsuta'),mapRegion='world', oceanCol = "white",landCol = "gray80")
 
 
